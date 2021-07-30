d974c4f44b22cfa452d2c507b07703b0c307a087
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2021-07-30 15:31:27 -0300

    Add SkipIterator

---
+++ SkipIterator/src/SkipIterator.test.ts
@@ -0,0 +1,34 @@
+import { assert } from 'chai'
+import { SkipIterator } from './SkipIterator'
+
+const testValues = [
+  {
+    toSkip: 5,
+    values: [2, 3, 5, 6, 5, 7, 5, -1, 5, 10],
+    // Empty strings are where .skip() is called (returns void)
+    results: [true, 2, '', 3, 6, 5, '', '', 7, -1, 10, false, ''],
+  },
+]
+
+for (const { values, results, toSkip } of testValues) {
+  const iterator = new SkipIterator(values)
+  const currentResults = [
+    iterator.hasNext,
+    iterator.next(),
+    iterator.skip(toSkip),
+    iterator.next(),
+    iterator.next(),
+    iterator.next(),
+    iterator.skip(toSkip),
+    iterator.skip(toSkip),
+    iterator.next(),
+    iterator.next(),
+    iterator.next(),
+    iterator.hasNext,
+    iterator.next(),
+  ]
+  assert.equal(currentResults.join(), results.join())
+}
+
+document.write(`<strong>✅ Test Passed: </strong>`)
+document.write(`<code>src/SkipIterator.test.ts</code>`)
---
+++ SkipIterator/src/SkipIterator.ts
@@ -0,0 +1,49 @@
+/**
+ * Iterator implementing skipping through .skip()
+ * @example
+ * const myIter = new SkipIterator([1,3,4,3,5])
+ * myIter.next() // 1
+ * myIter.skip(3)
+ * myIter.skip(3)
+ * myIter.next() // 4
+ * myIter.next() // 5
+ */
+export class SkipIterator {
+  /** Track current value for next() call */
+  private currentIndex = 0
+  /** Values to be skipped in order. First in, first out */
+  private skipQueue: number[] = []
+  // Iterator with values to be looped by next()
+  constructor(private iterator: number[]) {}
+  /** Returns next value to be skipped, or false */
+  get nextToSkip(): number | false {
+    // If there's no value to skip
+    if (!this.skipQueue.length) return false
+    // else return the value to skip
+    return this.skipQueue[0]
+  }
+  /* True if there are values to be looped by next () */
+  get hasNext(): boolean {
+    return this.iterator[this.currentIndex] !== undefined
+  }
+  /** Returns next not skipped value, or undefined */
+  next(): number | undefined {
+    const currentValue = this.iterator[this.currentIndex]
+    // Increment for upcoming next() calls
+    this.currentIndex++
+    // Check if there's a value to be skipped
+    const nextToSkip = this.nextToSkip
+    // Return current value if it should not be skipped
+    if (currentValue !== nextToSkip) return currentValue
+    // If current value should be skipped,
+    // remove it from the queue, and
+    // run the following next() call
+    this.skipQueue.shift()
+    // Call to get the next iterator value
+    return this.next()
+  }
+  /** Adds value to the end of the skip queue */
+  skip(value: number) {
+    this.skipQueue.push(value)
+  }
+}
