3ae5148761e3a678064167606f8eb15cb60f1d93
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2021-08-02 23:28:39 -0300

    Update Elevators file structure (prepare for Parcel)

--- Elevators/src/model/Context/react.ts
+++ Elevators/src/model/Context/react.ts
@@ -1,23 +1,28 @@
-/**
- * @file
- * @see {@link ./ContextProvider.tsx}
- * Context used by React UI tree
- */
 import { range } from 'lodash'
 import { createContext } from 'react'
 import { Elevator } from '../type/Elevator'
 import { ElevatorController } from '../type/Elevator/Controller'
 import { Floor } from '../type/Floor'
-import { ModelContext } from './model'
+import { ModelContext } from '../Context'
 
 const ctx = new ModelContext()
-/** 6 Floors from 0 to 5 */
+
+/**
+ * 6 floors, from 0 to 5
+ */
 const floors = range(6).map<Floor>((number) => new Floor(number, ctx))
-/** Context with 3 Elevators */
+
+/**
+ * Context with 3 elevators
+ */
 export const defaultValue = {
   elevatorService: new ElevatorController(
     range(3).map(() => new Elevator(floors, ctx)),
   ),
 }
 
+/**
+ * Context used by React UI tree
+ * @see {@link ModelContext}
+ */
 export const UiContext = createContext(defaultValue)
