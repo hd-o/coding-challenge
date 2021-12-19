7d7b51d245b892a55b287bf25d09c5fe8cfdb421
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2021-12-19 01:30:03 -0300

    chore(solid-calendar): remove unnecessary test describe block

--- solid-calendar/tests/calendar/view.test.tsx
+++ solid-calendar/tests/calendar/view.test.tsx
@@ -1,10 +1,8 @@
 import { getCalendarState } from './util/get/calendarState'
 import { renderCalendar } from './util/render/'
 
-describe('calendar view (initial rendering)', () => {
-  test('initial render snapshot', () => {
-    const calendar = renderCalendar()
-    expect(getCalendarState()).toMatchSnapshot('calendar-grid-cell-order')
-    expect(calendar.asFragment()).toMatchSnapshot('calendar-view')
-  })
+test('initial calendar render snapshot', () => {
+  const calendar = renderCalendar()
+  expect(getCalendarState()).toMatchSnapshot('calendar-grid-cell-order')
+  expect(calendar.asFragment()).toMatchSnapshot('calendar-view')
 })
