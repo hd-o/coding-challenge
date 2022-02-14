2fe7a0e93b1a22f530e2581c513398211102a384
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2022-02-14 00:58:01 -0300

    refactor(rx-elevators): simplify startup subscription

--- rx-elevators/modules/app/index.tsx
+++ rx-elevators/modules/app/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext, useEffect, useMemo } from 'react'
+import { FC, useContext, useEffect } from 'react'
 import { ElevatorContainerCtx } from '../elevator/container'
 import { SemanticUiContainerCtx } from '../pkg/semantic-ui/Container'
 import { useStartup } from '../startup'
@@ -7,12 +7,12 @@ import { useResolve } from '../util/useResolve'
 export const App: FC<{}> = () => {
   const Container = useContext(SemanticUiContainerCtx)
   const ElevatorContainer = useContext(ElevatorContainerCtx)
-
   const startup = useResolve(useStartup)
 
-  const modelSub = useMemo(() => startup(), [startup])
-
-  useEffect(() => () => modelSub.unsubscribe())
+  useEffect(() => {
+    const subscription = startup()
+    return () => subscription.unsubscribe()
+  })
 
   return (
     <div style={{ paddingTop: 20, minWidth: 600 }}>
