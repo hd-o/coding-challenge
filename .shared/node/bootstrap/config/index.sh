8a9007bbe893dd692014c23f9ae3f07b893d279e
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2022-02-17 19:59:23 -0300

    refactor: separate html bootstrap setup, and react eslint setup

---
+++ .shared/node/bootstrap/config/index.sh
@@ -0,0 +1,4 @@
+mkdir -p ./.config
+
+cp ../.shared/node/bootstrap/config/eslint.js.txt ./.config/eslint.js
+cp ../.shared/node/bootstrap/config/typescript.js.txt ./.config/typescript.js
--- .shared/node/bootstrap/react/index.sh
+++ .shared/node/bootstrap/react/index.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p ./.config
+
+cp ../.shared/node/bootstrap/react/config/eslint.js.txt ./.config/eslint.js
+
 yarn add \
   @testing-library/jest-dom \
   @testing-library/react \
