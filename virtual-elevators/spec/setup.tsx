016c8e32e4c6c17e0b8b48466135fe72cb042e3c
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2022-02-23 18:43:35 -0300

    refactor(virtual-elevators): only run necessary change in spec beforeEach

--- virtual-elevators/spec/setup.tsx
+++ virtual-elevators/spec/setup.tsx
@@ -10,49 +10,50 @@ import { FC, useEffect, useState } from 'react'
 import { container } from 'tsyringe'
 import { render } from '@testing-library/react'
 
-beforeEach(async () => {
+const lodash = container.resolve(Lodash)
+const rangeMap = lodash.rangeMap
+const processUtils = container.resolve(ProcessUtils)
+const rx = container.resolve(Rx)
+
+// Remove throttle wait time
+const _throttle = (fn: CallableFunction) => (...args: any[]) => fn(...args)
+const throttle = _throttle as typeof lodash.throttle
+
+// Remove process wait time
+const _createWaitProcess = () => () => true
+const createWaitProcess = _createWaitProcess as typeof processUtils.createWaitProcess
+
+// Manually control intervals
+const interval$ = new rx.Subject<void>()
+const animationFrames: typeof rx.animationFrames = () =>
+  interval$.pipe(rx.map(() => ({ timestamp: 0, elapsed: 0 })))
+
+// Elements required by elevator spec tests
+const TestCtrl: FC = () => {
+  const [intervalId, setIntervalId] = useState(-1)
+
+  // Clear interval timeout on unmount
+  useEffect(() => () => clearInterval(intervalId))
+
+  // Start interval timeout
+  const handleTestIntervalStart = (): void => {
+    if (intervalId !== -1) return
+    setIntervalId(window.setInterval(() => interval$.next(), 0))
+  }
+
+  // Render elements for spec test to click on
+  return <>
+    <div
+      data-testid='test-interval-start'
+      onClick={handleTestIntervalStart}
+    />
+  </>
+}
+
+beforeEach(() => {
   container.clearInstances()
-
-  const lodash = container.resolve(Lodash)
-  const rangeMap = lodash.rangeMap
-  const processUtils = container.resolve(ProcessUtils)
-  const rx = container.resolve(Rx)
-
-  // Remove throttle wait time
-  const _throttle = (fn: CallableFunction) => (...args: any[]) => fn(...args)
-  const throttle = _throttle as typeof lodash.throttle
-
-  // Remove process wait time
-  const _createWaitProcess = () => () => true
-  const createWaitProcess = _createWaitProcess as typeof processUtils.createWaitProcess
-
-  // Manually control intervals
-  const interval$ = new rx.Subject<void>()
-  const animationFrames: typeof rx.animationFrames = () =>
-    interval$.pipe(rx.map(() => ({ timestamp: 0, elapsed: 0 })))
-
   container.registerInstance(Lodash, { ...lodash, rangeMap, throttle })
   container.registerInstance(ProcessUtils, { ...processUtils, createWaitProcess })
   container.registerInstance(Rx, { ...rx, animationFrames })
-
-  // Render elements required by elevator spec tests
-  const TestCtrl: FC = () => {
-    const [intervalId, setIntervalId] = useState(-1)
-
-    useEffect(() => () => clearInterval(intervalId))
-
-    const handleTestIntervalStart = (): void => {
-      if (intervalId !== -1) return
-      setIntervalId(window.setInterval(() => interval$.next(), 0))
-    }
-
-    return <>
-      <div
-        data-testid='test-interval-start'
-        onClick={handleTestIntervalStart}
-      />
-    </>
-  }
-
   render(<App TestCtrl={TestCtrl} />)
 })
